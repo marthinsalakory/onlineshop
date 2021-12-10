036dc68625272efe58783c2f5ad9d2f867a72b0e
marthin salakory <marthinsalakory11@gmail.com>
2021-12-10 14:40:03 +0900

    kosongkan database

--- database/toko_online.sql
+++ database/toko_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Des 2021 pada 08.46
+-- Waktu pembuatan: 10 Des 2021 pada 06.39
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 8.0.12
 
@@ -37,16 +37,6 @@ CREATE TABLE `produk` (
   `deskripsi` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `produk`
---
-
-INSERT INTO `produk` (`id`, `penjual_id`, `nama_produk`, `berat_produk`, `harga`, `gambar`, `deskripsi`) VALUES
-('61adb1aae729f', 4, 'Mainan', 1, 50000, '61adb1aae7feb.jpg', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a moreorless normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'),
-('61adb710857b2', 5, 'Permainanku', 1, 30000, '61b1b17d65451.jpg', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or noncharacteristic words etc.'),
-('61adb7ad984ba', 6, 'Permainan Anak', 21, 500000, '61adb7ad999f1.jpg', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at HampdenSydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'),
-('61adbfa3950f0', 4, 'Desain Baju', 1, 100000, '61adbfa395d52.jpg', 'Baju yang sangat bagus, disesuaikan dengan gaya pakai ala modern, dan sangat cocok bagi anak muda jaman sekarang');
-
 -- --------------------------------------------------------
 
 --
@@ -60,14 +50,6 @@ CREATE TABLE `proses` (
   `jumlah_barang` int(11) NOT NULL,
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `proses`
---
-
-INSERT INTO `proses` (`id`, `kode_produk`, `pembeli_id`, `jumlah_barang`, `status`) VALUES
-(2, '61adb710857b2', 4, 2, 1),
-(3, '61adb7ad984ba', 4, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -89,15 +71,6 @@ CREATE TABLE `users` (
   `kota` varchar(255) NOT NULL,
   `kode_pos` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `users`
---
-
-INSERT INTO `users` (`id`, `nama_pengguna`, `kata_sandi`, `nama_lengkap`, `foto_profil`, `no_telp`, `nama_jalan`, `rt`, `rw`, `kelurahan`, `kota`, `kode_pos`) VALUES
-(4, 'alfreinsco', 'alfreinsco', 'Marthin Alfreinsco Salakory', '61abfc797afb8.jpg', '081318812027', 'Jl.Pattimura', '11', '21', 'Lateri', 'Ambon', '12345'),
-(5, 'alfreinsco1', 'alfreinsco1', 'alfreinsco1', '61adb6c278e56.jpg', '0812', 'Jl.Pattimura', '1', '2', 'Lateri', 'Ambon', '23432'),
-(6, 'alfreinsco2', 'alfreinsco2', 'alfreinsco2', '61adb76561912.jpg', '12', 'Jl.Pattimura', '12', '32', 'Lateri', 'Ambon', '32454');
 
 --
 -- Indexes for dumped tables
@@ -129,13 +102,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `proses`
 --
 ALTER TABLE `proses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
